14677e71179c0c0a166b658c5d16ef3604663279
Jørgen <mail@jvik.no>
2018-12-20 13:53:15 +0100

    Modified to work with TypeScript

---
+++ index.ts
@@ -0,0 +1,63 @@
+import { SchemaDirectiveVisitor } from "apollo-server-express";
+import { defaultFieldResolver } from "graphql";
+import strategies from "./strategies";
+
+class AuthDirective extends SchemaDirectiveVisitor {
+  public visitObject(type) {
+    this.ensureFieldsWrapped(type);
+    type._requiredAuthRole = this.args.requires;
+  }
+  // Visitor methods for nested types like fields and arguments
+  // also receive a details object that provides information about
+  // the parent and grandparent types.
+  public visitFieldDefinition(field, details) {
+    this.ensureFieldsWrapped(details.objectType);
+    field._requiredAuthRole = this.args.requires;
+  }
+
+  public ensureFieldsWrapped(objectType) {
+    // Mark the GraphQLObjectType object to avoid re-wrapping:
+    if (objectType._authFieldsWrapped) {
+      return;
+    }
+    objectType._authFieldsWrapped = true;
+
+    const fields = objectType.getFields();
+
+    Object.keys(fields).forEach((fieldName) => {
+      const field = fields[fieldName];
+      const {
+        resolve = defaultFieldResolver
+      } = field;
+      field.resolve = async function (...args) {
+        // Get the required Role from the field first, falling back
+        // to the objectType if no Role is required by the field:
+        const requiredRole =
+          field._requiredAuthRole || objectType._requiredAuthRole;
+
+        if (!requiredRole) {
+          // Let's be on the safe side
+          throw new Error("not authorized");
+        }
+
+        const requestData = args[2];
+        await this.executeStrategy(requiredRole, requestData);
+
+        return resolve.apply(this, args);
+      }.bind(this);
+    });
+  }
+
+  public async executeStrategy(role, requestData) {
+    // console.log(role);
+    // TODO: Resolve strategy call
+    const strategyResult = await strategies[role.toLowerCase()](requestData);
+
+    console.log(`Strategy result: ${strategyResult}`);
+    if (!strategyResult) {
+      throw new Error("not authorized");
+    }
+  }
+}
+
+export default AuthDirective;
